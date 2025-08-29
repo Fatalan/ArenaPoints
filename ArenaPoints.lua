2233898c3cf917a58f3d959baff53a62c27ff0b5
Fatalan <lilith11@yandex.ru>
2025-08-29 08:08:42 +0300

    Add more text info

--- ArenaPoints.lua
+++ ArenaPoints.lua
@@ -1,15 +1,17 @@
 local localization = {
 	ruRU = {
-		REASON_NOT_QUALIFIED = "потому, что вы не отыграли 10 игр в этом брекете.",
+		REASON_NOT_QUALIFIED = "потому, что вы не отыграли 10 игр в этом брекете на этой неделе.",
 		REASON_NOT_ENOUGH_PERCENT = "потому, что вы сыграли менее 30% игр в этой команде на этой неделе",
 		WILL_EARN = "Вы получите %i очков арены.",
-		WONT_EARN = "Вы не получите очков арены "
+		WONT_EARN = "Вы не получите очков арены ",
+		AP_INFO = "Помните, вы получите награду только за один из брекетов, конкретно за тот, за который вы получите больше всего очков арены"
 	},
 	enUS = {
-		REASON_NOT_QUALIFIED = "'cause you haven't participated in 10 arena matches in this bracket.",
+		REASON_NOT_QUALIFIED = "'cause you haven't participated in 10 arena matches in this bracket this week.",
 		REASON_NOT_ENOUGH_PERCENT = "'cause you have participated in less then 30% of this team matches this week",
 		WILL_EARN = "You'll earn %i arena points.",
-		WONT_EARN = "You won't earn any arena points "
+		WONT_EARN = "You won't earn any arena points ",
+		AP_INFO = "Remember, you'll only get reward for one of the brackets, specifically for the bracket you'll earn most arena points from"
 	}
 }
 local pvpTeamFrames = {PVPTeam1, PVPTeam2, PVPTeam3}
@@ -64,6 +66,21 @@ local function SetupPVPTeamFrame(frame)
         self.AwaitedAP:SetText(text)
     end)
 end
+
+PVPFrame:HookScript("OnShow", function(self)
+		if not self.APInfo then
+			self.APInfo = self:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+			self.APInfo:SetFont("Fonts\\FRIZQT__.TTF", 8, "OUTLINE")
+			self.APInfo:SetPoint("CENTER", self, "TOP", 0, -162)
+			self.APInfo:SetTextColor(1, 1, 1, 1)
+			self.APInfo:SetSize(self:GetWidth() - 50, 0)
+			local locale = GetLocale()
+			if(localization[locale] == nil) then
+				locale = "enUS"
+			end
+			self.APInfo:SetText(localization[locale]["AP_INFO"])
+		end
+	end)
 
 for i, frame in ipairs(pvpTeamFrames) do
     SetupPVPTeamFrame(frame)
